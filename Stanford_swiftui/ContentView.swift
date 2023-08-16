59e5dabaa0b049858a3a4ccf934af5c22e023ad7
Milos Savkovic <milos.savkovic.somias@gmail.com>
2023-08-16 17:18:28 +0200

    - Create a function cardCountAdjuster to reuse tapping logic on adding/removing cards

--- Stanford_swiftui/ContentView.swift
+++ Stanford_swiftui/ContentView.swift
@@ -36,79 +36,74 @@ struct ContentView: View {
         }.padding()
     }
     
-    var cardRemover: some View {
+    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
         Button {
-            if cardCount > 1 {
-                cardCount -= 1
-            }
+            cardCount += offset
         } label: {
-            Image(systemName: "rectangle.stack.badge.minus.fill")
+            Image(systemName: symbol)
         }
         .imageScale(.large)
         .font(.largeTitle)
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+    }
+    
+    var cardRemover: some View {
+        cardCountAdjuster(by: -1, symbol: "rectangle.stack.badge.minus.fill")
     }
     
     var cardAdder: some View {
-        Button {
-            if cardCount < emojis.count {
-                cardCount += 1
-            }
-        } label: {
-            Image(systemName: "rectangle.stack.badge.plus.fill")
-        }
-        .imageScale(.large)
-        .font(.largeTitle)
+        cardCountAdjuster(by: 1, symbol: "rectangle.stack.badge.plus.fill")
     }
 }
-
-struct CardView: View {
-    // Views are immutable and to change the value I need to use @State variable.
-    @State var isFaceUp = false
-    let content: String
     
-    var body: some View {
-        ZStack {
-            // Reason I can use variable like this is because of ViewBuilder
-            // let base: RoundedRectangle = RoundedRectangle(cornerRadius: 12)
-            // But beacuse of "Type Inference" we don't need to explicitly write Type.
-            let base = RoundedRectangle(cornerRadius: 12)
-            
-            // But ViewBuilder only can do If's (conditionals, also Switch), lists and local variables.
-            
-            if isFaceUp {
-                base.fill(.white)
-                base.strokeBorder(lineWidth: 2)
-                Text(content).font(.largeTitle)
-            } else {
-                base.fill()
+    struct CardView: View {
+        // Views are immutable and to change the value I need to use @State variable.
+        @State var isFaceUp = false
+        let content: String
+        
+        var body: some View {
+            ZStack {
+                // Reason I can use variable like this is because of ViewBuilder
+                // let base: RoundedRectangle = RoundedRectangle(cornerRadius: 12)
+                // But beacuse of "Type Inference" we don't need to explicitly write Type.
+                let base = RoundedRectangle(cornerRadius: 12)
+                
+                // But ViewBuilder only can do If's (conditionals, also Switch), lists and local variables.
+                
+                if isFaceUp {
+                    base.fill(.white)
+                    base.strokeBorder(lineWidth: 2)
+                    Text(content).font(.largeTitle)
+                } else {
+                    base.fill()
+                }
+            }.onTapGesture {
+                // onTapGesture has argument "count" which decides how many times View needs to be tapped to execute onTapGesture function. Default count is 1.
+                isFaceUp.toggle()
             }
-        }.onTapGesture {
-            // onTapGesture has argument "count" which decides how many times View needs to be tapped to execute onTapGesture function. Default count is 1.
-            isFaceUp.toggle()
         }
     }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
-}
