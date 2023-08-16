e181e153c11171059c50452ae7989ff38ac59e94
Milos Savkovic <milos.savkovic.somias@gmail.com>
2023-08-16 12:35:10 +0200

    Initial Commit

---
+++ Stanford_swiftui/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Stanford_swiftui
+//
+//  Created by Milos on 16. 8. 2023..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Stanford_swiftui/Stanford_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stanford_swiftuiApp.swift
+//  Stanford_swiftui
+//
+//  Created by Milos on 16. 8. 2023..
+//
+
+import SwiftUI
+
+@main
+struct Stanford_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
