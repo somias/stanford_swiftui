4ee96843a9fa72aef0cc7ce2b5126637010a198c
Milos Savkovic <milos.savkovic.somias@gmail.com>
2023-08-16 14:57:30 +0200

    - Togglable isFaceUp

--- Stanford_swiftui/ContentView.swift
+++ Stanford_swiftui/ContentView.swift
@@ -9,15 +9,60 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        HStack {
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
         }
+        .foregroundColor(.orange)
         .padding()
     }
 }
+
+struct CardView: View {
+    // Views are immutable and to change the value I need to use @State variable.
+    @State var isFaceUp = false
+    
+    var body: some View {
+        ZStack {
+            // Reason I can use variable like this is because of ViewBuilder
+            // let base: RoundedRectangle = RoundedRectangle(cornerRadius: 12)
+            // But beacuse of "Type Inference" we don't need to explicitly write Type.
+            let base = RoundedRectangle(cornerRadius: 12)
+            
+            // But ViewBuilder only can do If's (conditionals, also Switch), loops and variable assignments.
+            
+            if isFaceUp {
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
+                Text("👻").font(.largeTitle)
+            } else {
+                base.fill()
+            }
+        }.onTapGesture {
+            isFaceUp.toggle()
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
