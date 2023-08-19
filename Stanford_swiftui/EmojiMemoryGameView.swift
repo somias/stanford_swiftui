4cef79c541cb5609487024923ae3b004fd470c2a
Milos Savkovic <milos.savkovic.somias@gmail.com>
2023-08-19 13:42:54 +0200

    - Make Animation when shuffling cards - Work with Equatable, Identifiable protocols - Learn about conforming Generics to Protocol: where SomeType: Equatable

--- Stanford_swiftui/EmojiMemoryGameView.swift
+++ Stanford_swiftui/EmojiMemoryGameView.swift
@@ -15,8 +15,9 @@ struct EmojiMemoryGameView: View {
         VStack {
             ScrollView {
                 cards
-                    .scrollIndicators(.hidden)
-            }
+                    .animation(.default, value: viewModel.cards)
+                    
+            }.scrollIndicators(.hidden)
             Button("Shuffle") {
                 viewModel.shuffle()
             }
@@ -25,8 +26,8 @@ struct EmojiMemoryGameView: View {
     
     var cards: some View {
         LazyVGrid(columns: [GridItem(.adaptive(minimum: 85), spacing: 0)], spacing: 0) {
-            ForEach(viewModel.cards.indices, id: \.self) { index in
-                CardView(viewModel.cards[index])
+            ForEach(viewModel.cards) { card in
+                CardView(card)
                     .aspectRatio(2/3, contentMode: .fit)
                     .padding(4)
             }
--- Stanford_swiftui/MemoryGame.swift
+++ Stanford_swiftui/MemoryGame.swift
@@ -8,7 +8,8 @@
 import Foundation
 
 // CardContent is Generic (Generics are part of Swift language)
-struct MemoryGame<CardContent> {
+// Because CardContent is Generic ("we don't care for Type), because of our use of Equatable protocol in Card struct we need to implement Equatable for CardContent too. And the way to do that is like this: where CardContent: Equatable
+struct MemoryGame<CardContent> where CardContent: Equatable {
     private(set) var cards: Array<Card>
     
     init(numberOfPairsOfCard: Int, cardContentFactory: (Int) -> CardContent) {
@@ -17,8 +18,8 @@ struct MemoryGame<CardContent> {
         // max(2, numberOfPairsOfCard) - at least 2 pairs of cards
         for pairIndex in 0..<max(2, numberOfPairsOfCard) {
             let cardContent = cardContentFactory(pairIndex)
-            cards.append(Card(content: cardContent))
-            cards.append(Card(content: cardContent))
+            cards.append(Card(content: cardContent, id: "\(pairIndex + 1)a"))
+            cards.append(Card(content: cardContent, id: "\(pairIndex + 1)b"))
         }
     }
 
@@ -31,9 +32,16 @@ struct MemoryGame<CardContent> {
         print(cards)
     }
     
-    struct Card {
+    struct Card: Equatable, Identifiable {
+        
+        // In Swift, when comparing each thing we don't need it because Swift will do it for us. So I will comment this code.
+//        static func == (lhs: Card, rhs: Card) -> Bool {
+//            return lhs.isFaceUp == rhs.isFaceUp && lhs.isMatched == rhs.isMatched && lhs.content == rhs.content
+//        }
         var isFaceUp = true
         var isMatched = false
         let content: CardContent
+        
+        var id: String
     }
 }
